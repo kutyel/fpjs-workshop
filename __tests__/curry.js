ba639a35960bce64dbdcba1a90c191e7d1ab1229
kutyel <flaviocorpa@gmail.com>
2019-09-30 08:47:28 +0200

    Fix regex exercise with curry 🍛

--- __tests__/curry.js
+++ __tests__/curry.js
@@ -1,8 +1,8 @@
-import { curry, split, map, filter, reduce, identity, match } from 'ramda'
+import { curry, split, map, filter, reduce, identity, test } from 'ramda'
 
 describe('Curry 🍛', () => {
   // Exercise 1
-  test('Refactor to remove all arguments by partially applying the function.', () => {
+  it('Refactor to remove all arguments by partially applying the function.', () => {
     // words :: String -> [String]
     const words = str => split(' ', str) // TODO:
     expect(words('Jingle bells Batman smells')).toEqual(['Jingle', 'bells', 'Batman', 'smells'])
@@ -10,7 +10,6 @@ describe('Curry 🍛', () => {
     // Exercise 1a
     // Use map to make a new words fn that works on an array of strings.
     const sentences = identity // TODO:
-
     expect(sentences(['Jingle bells Batman smells', 'Robin laid an egg'])).toEqual([
       ['Jingle', 'bells', 'Batman', 'smells'],
       ['Robin', 'laid', 'an', 'egg'],
@@ -18,9 +17,9 @@ describe('Curry 🍛', () => {
   })
 
   // Exercise 2
-  test('Refactor to remove all arguments by partially applying the functions.', () => {
+  it('Refactor to remove all arguments by partially applying the functions.', () => {
     // filterQs :: [String] -> [String]
-    const filterQs = xs => filter(x => match(/q/i, x), xs) // TODO:
+    const filterQs = xs => filter(x => test(/q/i, x), xs) // TODO:
     expect(filterQs(['quick', 'camels', 'quarry', 'over', 'quails'])).toEqual([
       'quick',
       'quarry',
@@ -29,7 +28,7 @@ describe('Curry 🍛', () => {
   })
 
   // Exercise 3
-  test('Use the helper function _keepHighest to refactor max to not reference any arguments.', () => {
+  it('Use the helper function _keepHighest to refactor max to not reference any arguments.', () => {
     // LEAVE BE:
     const _keepHighest = (x, y) => (x >= y ? x : y)
     // TODO: REFACTOR THIS ONE:
@@ -39,7 +38,7 @@ describe('Curry 🍛', () => {
   })
 
   // Bonus 1
-  test("Wrap array's slice to be functional and curried.", () => {
+  it("Wrap array's slice to be functional and curried.", () => {
     // [1, 2, 3].slice(0, 2)
     const slice = Array.prototype.slice // TODO:
     expect(slice(1)(3)(['a', 'b', 'c'])).toEqual(['b', 'c'])
